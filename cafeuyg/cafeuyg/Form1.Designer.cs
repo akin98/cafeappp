7df87b1fc8240265db493e80ac8dec27e7d96a8f
akın sultar <109798864+akin98@users.noreply.github.com>
2022-07-22 14:50:26 +0300

    cafeapp

---
+++ cafeuyg/cafeuyg/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace cafeuyg
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttoncyartı = new System.Windows.Forms.Button();
+            this.buttoncyeksi = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(75, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "çay";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "12";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(434, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "ödenecek tutar";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(434, 55);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(15, 16);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "--";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(279, 13);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(33, 16);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "ekle";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(339, 13);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(36, 16);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "çıkar";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 68);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "siparişi tamamla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttoncyartı
+            // 
+            this.buttoncyartı.Location = new System.Drawing.Point(271, 50);
+            this.buttoncyartı.Name = "buttoncyartı";
+            this.buttoncyartı.Size = new System.Drawing.Size(52, 21);
+            this.buttoncyartı.TabIndex = 11;
+            this.buttoncyartı.Text = "+";
+            this.buttoncyartı.UseVisualStyleBackColor = true;
+            this.buttoncyartı.Click += new System.EventHandler(this.buttoncyartı_Click);
+            // 
+            // buttoncyeksi
+            // 
+            this.buttoncyeksi.Location = new System.Drawing.Point(342, 49);
+            this.buttoncyeksi.Name = "buttoncyeksi";
+            this.buttoncyeksi.Size = new System.Drawing.Size(45, 22);
+            this.buttoncyeksi.TabIndex = 12;
+            this.buttoncyeksi.Text = "-";
+            this.buttoncyeksi.UseVisualStyleBackColor = true;
+            this.buttoncyeksi.Click += new System.EventHandler(this.buttoncyeksi_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "adet";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(219, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(75, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 18);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "kahve";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(160, 101);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 16);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "20";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(219, 101);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(14, 16);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(271, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 24);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(342, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 24);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(437, 93);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(11, 16);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "-";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(150, 13);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(31, 16);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "fiyat";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(342, 143);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(48, 16);
+            this.label14.TabIndex = 22;
+            this.label14.Text = "toplam";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(437, 142);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(11, 16);
+            this.label15.TabIndex = 23;
+            this.label15.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttoncyeksi);
+            this.Controls.Add(this.buttoncyartı);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttoncyartı;
+        private System.Windows.Forms.Button buttoncyeksi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+    }
+}
+
---
+++ cafeuyg/cafeuyg/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cafeuyg
+{
+    public partial class Form1 : Form
+    {
+        int cay = 0, kahve = 0, kola = 0;
+
+        private void buttoncyeksi_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(label4.Text);
+            sayi--;
+            cay = sayi;
+            label4.Text = cay.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         string a= label8.Text;
+            int b= Convert.ToInt32(label2.Text);
+            int c= Convert.ToInt32(label4.Text);
+            a = (b * c).ToString();
+            label8.Text = a;
+
+            string k = label12.Text;
+            int d = Convert.ToInt32(label7.Text);
+            int f = Convert.ToInt32(label11.Text);
+            k = (d * f).ToString();
+            label12.Text = k;
+
+            string t = label15.Text;
+            t = (a+k).ToString();
+            label15.Text = t;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(label11.Text);
+            sayi++;
+            kahve = sayi;
+            label11.Text = kahve.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(label11.Text);
+            sayi--;
+            kahve = sayi;
+            label11.Text = kahve.ToString();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttoncyartı_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(label4.Text);
+            sayi++;
+            cay = sayi;
+            label4.Text = cay.ToString();
+        }
+    }
+}
